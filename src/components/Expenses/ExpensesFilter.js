12496d429d336ef7b4461572b454f70f9da68a67
Vaskar Chandra Das <vcdas123@gmail.com>
2023-07-25 01:47:47 +0530

    up

--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import './ExpensesFilter.css';
 
-const ExpensesFilter = props => {
-  const dropDownChangeHandler = e => {
-    props.onChangeFilter(e.target.value);
-  };
-  // Select tag here is recieving value from the parent as well as it passing the value to the parent on change - This is called two way binding.
+const ExpensesFilter = ({ onChangeFilter, selected }) => {
+  const currentYear = +new Date().getFullYear();
+  const dateOptions = [currentYear, currentYear - 1, currentYear - 2];
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select value={props.selected} onChange={dropDownChangeHandler}>
-          <option value="2023">2023</option>
-          <option value="2022">2022</option>
-          <option value="2021">2021</option>
-          <option value="2020">2020</option>
+        <select value={selected} onChange={e => onChangeFilter(e.target.value)}>
+          {dateOptions.map(item => (
+            <option value={item}>{item}</option>
+          ))}
         </select>
       </div>
     </div>
